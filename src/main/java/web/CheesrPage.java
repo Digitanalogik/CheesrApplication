f3b83f30b4d2a341a7da80987b6d31e7ea4eda1e
Tatu Soininen <tatu.soininen@cinia.fi>
2019-03-16 17:10:25 +0200

    Add CheesPage - A Base Class To Derive Other Pages From

--- src/main/java/web/CheesrPage.java
+++ src/main/java/web/CheesrPage.java
@@ -1,4 +1,21 @@
 package web;
 
-public class CheesrPage {
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public abstract class CheesrPage extends WebPage {
+
+    public CheesrSession getCheesrSession() {
+        return (CheesrSession) getSession();
+    }
+
+    public Cart getCart() {
+        return getCheesrSession().getCart();
+    }
+
+    public List<Cheese> getCheeses() {
+        return CheesrApplication.get().getCheeses();
+    }
+
 }
